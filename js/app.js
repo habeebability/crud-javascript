1f4c25f9bc5a882de1d328b4ec8782e42a8a1639
Habeebability <awoyemi.habeeb@gmail.com>
2022-04-19 20:43:31 +0100

    fixed edit button issue on the table

--- js/app.js
+++ js/app.js
@@ -15,7 +15,8 @@ let elname = document.querySelector("#elname");
 let edepartment = document.querySelector("#edepartment");
 let esalary = document.querySelector("#esalary");
 const form = document.querySelector("#form");
-const eform = document.querySelector("#form");
+const eform = document.querySelector("#eform");
+const deleteRow = document.getElementsByClassName("btnDelete");
 
 addBtn.addEventListener("click", () => {
   addModal.style.display = "block";
@@ -28,43 +29,28 @@ closeBtnAdd.addEventListener("click", () => {
 closeBtnEdit.addEventListener("click", () => {
   editModal.style.display = "none";
 });
-
 let sn = 0;
 let users = [];
 
-const usersString = localStorage.getItem("users");
-const edittedString = localStorage.getItem("edittedUsers");
-
-try {
-  if (usersString) {
-    users = JSON.parse(usersString);
+function starter() {
+  sn = 0;
+  const usersString = localStorage.getItem("users");
+  console.log(usersString);
+  try {
+    if (usersString) {
+      users = JSON.parse(usersString);
+    }
+  } catch (e) {
+    console.log(e);
   }
-} catch (e) {
-  console.log(e);
+
+  users.forEach((user) => {
+    addRow(user);
+  });
 }
-
-users.forEach((user) => {
-  addRow(user);
-});
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const user = {
-    firstName: fname.value,
-    lastName: lname.value,
-    salary: salary.value,
-    department: department.value,
-  };
-
-  addRow(user);
-  users.push(user);
-  localStorage.setItem("users", JSON.stringify(users));
-});
 
 function addRow({ firstName, lastName, department, salary }) {
   let row = table.insertRow(-1);
-
   const snRow = row.insertCell(0);
   const finame = row.insertCell(1);
   const laname = row.insertCell(2);
@@ -78,32 +64,19 @@ function addRow({ firstName, lastName, department, salary }) {
   laname.innerHTML = lastName;
   dmt.innerHTML = department;
   sal.innerHTML = `$${salary}`;
-  del.innerHTML = `<i class="ri-delete-bin-6-line"></i>`;
-  edit.innerHTML = `<i class="ri-pencil-line"></i>`;
+  del.innerHTML = `<i class="ri-delete-bin-6-line btnEdit"></i>`;
+  edit.innerHTML = `<i class="ri-pencil-line" btnDelete></i>`;
 
   const index = sn;
+  addModal.style.display = "none";
 
-  // edit.forEach((user)=> {
+  fname.value = " ";
+  lname.value = " ";
+  department.value = " ";
+  salary.value = " ";
 
-  // })
-  edit.addEventListener("click", () => {
-    // tbody.innerHTML = ' ';
-    editModal.style.display = "block";
-
-    efname.value = users[index].firstName;
-    elname.value = users[index].lastName;
-    esalary.value = users[index].salary;
-    edepartment.value = users[index].department;
-
-    eform.addEventListener("submit", () => {
-      let user = {
-        firstName: efname.value,
-        lastName: elname.value,
-        salary: esalary.value,
-        department: edepartment.value,
-      };
-    });
-  });
+  tbody.appendChild(row);
+  sn++;
 
   del.addEventListener("click", () => {
     tbody.innerHTML = " ";
@@ -116,13 +89,45 @@ function addRow({ firstName, lastName, department, salary }) {
     console.log(users);
   });
 
-  addModal.style.display = "none";
+  edit.addEventListener("click", () => {
+    // tbody.innerHTML = " ";
+    editModal.style.display = "block";
 
-  fname.value = " ";
-  lname.value = " ";
-  department.value = " ";
-  salary.value = " ";
+    efname.value = users[index].firstName;
+    elname.value = users[index].lastName;
+    esalary.value = users[index].salary;
+    edepartment.value = users[index].department;
 
-  tbody.appendChild(row);
-  sn++;
+    eform.addEventListener("submit", (e) => {
+      e.preventDefault();
+      users[index].firstName = efname.value;
+      users[index].lastName = elname.value;
+      users[index].salary = esalary.value;
+      users[index].department = edepartment.value;
+      localStorage.removeItem("users");
+      tbody.innerHTML = " ";
+      localStorage.setItem("users", JSON.stringify(users));
+      starter();
+      editModal.style.display = "none";
+    });
+  });
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const user = {
+    firstName: fname.value,
+    lastName: lname.value,
+    salary: salary.value,
+    department: department.value,
+  };
+  addRow(user);
+  users.push(user);
+  // localStorage.removeItem("user");
+  localStorage.setItem("users", JSON.stringify(users));
+});
+
+window.onload = function () {
+  starter();
+};
